706538bbb1cc1c52ee0f20cddb52d5ab69ceb1f2
Kunuut Kaali <kunuutkaali@gmail.com>
2023-05-26 09:01:09 +0200

    Display product, api implemented

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MyStore CMS' });
 });
+/* TEST API */
+router.get('/test', async(req, res, next)=>{
+  try {
+    const response = await axios.get(`http://localhost:3000/test`);
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+    // Handle errors
+  }
+})
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
+const axios = require('axios');
+const apiBaseUrl = "http://localhost:3000"
+
 /* GET products listing. */
-router.get('/', function(req, res, next) {
-  res.render('products/products', { title: "My products"})
+router.get('/', async function(req, res, next) {
+  /* Get products from API */
+  let params = req.query && req.query
+  let data = {}
+  try {
+    let categoryUrl = apiBaseUrl + '/categories'
+    const categoriesResponse = await axios.get(categoryUrl)
+    data.categories = categoriesResponse.data;
+    try {
+      let productsUrl = apiBaseUrl + '/products'
+      const productsResponse = await axios.get(productsUrl, { params });
+      data.products = productsResponse.data
+      res.render('products/products', { title: "My products", data:data})
+    } catch (error) {
+      res.status(400).json(error.code);
+    }
+  } catch (error) {
+    res.status(400).json(error.code);
+  }
 });
 
+
+/* view single product */
+router.get('/product', async(req, res, next)=>{
+  let params = req.query
+  let url = apiBaseUrl + '/products/product'
+  try {
+    const response = await axios.get(url, { params });
+    if(response.data.length > 0){
+      let data = {product:response.data[0]}
+      res.render('products/product', {data: data})
+    }else{
+      res.render('products/product', {data: {}})
+    }
+  } catch (error) {
+    res.status(400).json(error.code);
+  }
+})
+
 router.get('/categories/', (req, res, next)=>{
-  res('Respond with a resource ');
+  res.send('Respond with a resource ');
 })
 
 module.exports = router;
